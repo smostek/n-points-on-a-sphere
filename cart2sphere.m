fb008a776d6f99b8f7022032b70ba5d0a0972e48
Sebastian Mostek <162070478+smostek@users.noreply.github.com>
2024-07-31 15:26:06 -0400

    Delete cart2sphere.m

--- cart2sphere.m
+++
@@ -1,7 +0,0 @@
-function S = cart2sphere(C)
-%Converts Cartesian to Spherical
-%C is an n-by-3 matrix of the x,y,&z components of n points
-%S is an n-by-3 matrix of the r,theta,&phi components of those same points
-% also works with 3D matrices
-r = sqrt(sum(C.^2,2));
-S = [r, acos(C(:,3,:)./r), mod(atan2(C(:,2,:),C(:,1,:)),2*pi)];
