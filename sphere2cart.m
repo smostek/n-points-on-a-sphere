5f4d1b199c198708964bb2355b46978e9973cb7e
Sebastian Mostek <162070478+smostek@users.noreply.github.com>
2024-07-31 15:30:45 -0400

    Delete sphere2cart.m

--- sphere2cart.m
+++
@@ -1,8 +0,0 @@
-function C = sphere2cart(S)
-% Converts from Spherical to Cartesian
-% S is an n-by-3 matrix of r,theta,&phi components of n points
-% C is an n-by-3 matrix of x,y,&z components for those same points
-% also works with a 3D array.
-C = S(:,1,:).*[sin(S(:,2,:)).*cos(S(:,3,:)), ...
-               sin(S(:,2,:)).*sin(S(:,3,:)), ...
-               cos(S(:,2,:))                ];
