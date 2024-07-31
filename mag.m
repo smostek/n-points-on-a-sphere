87195cd5f1905f67b7d022aaf6eb930050599585
Sebastian Mostek <162070478+smostek@users.noreply.github.com>
2024-07-31 15:29:44 -0400

    Delete mag.m

--- mag.m
+++
@@ -1,9 +0,0 @@
-function m = mag(C,dir)
-% magnitude
-% each row of C is a cartesian vector,
-% m gives the magnitudes of those vectors
-arguments
-    C    double
-    dir  double= 2
-end
-m = sqrt(sum(C.^2,dir));
